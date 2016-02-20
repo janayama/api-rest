b3d30159f5c098b65e0ce298baa349444155c743
janayama <javier.anaya.martinez@gmail.com>
2016-02-20 15:43:58 +0100

    get access token helper

--- app/api/concerns/api_guard.rb
+++ app/api/concerns/api_guard.rb
@@ -4,12 +4,23 @@ module APIGuard
   extend ActiveSupport::Concern
 
   included do
+    	# OAuth2 Resource Server Authentication
+    	use Rack::OAuth2::Server::Resource::Bearer, 'The API' do |request|
+      request.access_token
+    end
+
     helpers HelperMethods
   end
 
   # Helper Methods for Grape Endpoint
   module HelperMethods
-  end
+		private
+		def get_token_string
+			# The token was stored after the authenticator was invoked.
+			# It could be nil. The authenticator does not check its existence.
+			request.env[Rack::OAuth2::Server::Resource::ACCESS_TOKEN]
+		end
+	end
 
   module ClassMethods
   end
