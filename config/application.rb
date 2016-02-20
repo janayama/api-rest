e4a3637b26cbaf13d3e3e44400fcde366618f00b
janayama <javier.anaya.martinez@gmail.com>
2016-02-20 13:14:39 +0100

    load /app/api/

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module ApiRest
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.paths.add "app/api", glob: "**/*.rb"
+    config.autoload_paths += Dir["#{Rails.root}/app/api/*"]
   end
 end
